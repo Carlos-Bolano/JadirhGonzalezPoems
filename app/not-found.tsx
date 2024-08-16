1aa985b99f6f6c138e2261122b1e8ece94920f06
carlos-bolano <carlostutos828@gmail.com>
2024-08-15 22:11:17 -0500

    feat: remuve some unuseful code

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,5 @@
-import { Button } from "@/components/ui/Button";
 import { LinkButton } from "@/components/ui/LinkButton";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function NotFound() {
   return (
